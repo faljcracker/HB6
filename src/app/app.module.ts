1f84e2fda978e29d7107a9896098258fa2dfe4c6
nSedrickm <nsedrick101@gmail.com>
2020-04-25 22:43:06 +0100

    add maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +18,9 @@ import { MapsComponent } from './maps/maps.component';
   ],
   imports: [
     BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAzSnXXXXXXXXXXXXXXXXXSZGGWU'
+    }),
     AppRoutingModule
   ],
   providers: [],
--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapsComponent implements OnInit {
 
+  title: string = 'AGM project';
+  latitude: number;
+  longitude: number;
+  zoom: number;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // Get Current Location Coordinates
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 15;
+      });
+    }
   }
 
 }
