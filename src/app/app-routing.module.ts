370547eab45e2fe113bfd089c8fa148e32f4d439
nSedrickm <nsedrick101@gmail.com>
2020-04-28 22:07:52 +0100

    nav routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { TrackingComponent } from './tracking/tracking.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'Home', component: AppComponent },
+  { path: 'Tracking', component: TrackingComponent },
+  { path: 'Services', component: ServicesComponent },
+  { path: 'About', component: AboutComponent },
+  { path: 'Contact', component: ContactComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
